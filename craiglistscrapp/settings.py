dbe539888056e57e697adcf7d459add98fdb2dd8
KarimGil <karimg87@hotmail.com>
2020-01-18 07:43:16 -0800

    Add precompiled assets for Heroku

--- craiglistscrapp/settings.py
+++ craiglistscrapp/settings.py
@@ -16,12 +16,14 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b6i61wz3+9*fhdbk4^qe6@!(!v3v!^$x2#np81fxk^zrvurntw'
+#SECRET_KEY = 'b6i61wz3+9*fhdbk4^qe6@!(!v3v!^$x2#np81fxk^zrvurntw'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,8 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
